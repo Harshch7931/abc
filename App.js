f0e27690589e42d9b367533038f888e942ca451f
Harshch7931 <harshch7o931@gmail.com>
2019-05-24 01:15:23 +0530

    added new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js fileadded new js file

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+Stylesheet,
+Text,
+View
+} 
+from 'react-native';
+export defalut class extends React.Component{
+  render(){
+    return (
+      <View style={styles.container}>
+       <Text> Open up App.jssdlfhl</Text>
+
+      </View>
+      );
+  }
+  
+}
+const styles={
+  container:{
+   flex: 1,
+   marginTop: 30 
+  }
+  
+
+}
